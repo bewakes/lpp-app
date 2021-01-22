2c2521799770974a5885775225a64c570211b2bf
bewakes <bewakepandey@gmail.com>
2021-01-22 09:48:11 +0545

    Add CORS handling

--- app/Main.hs
+++ app/Main.hs
@@ -12,6 +12,10 @@ main = do
     scotty 4000 $ do
         get "/" $ do
             html "<h1>This is from scotty</h1>"
+        options "/" $ do
+            setHeader "Access-Control-Allow-Origin" "*"
+            setHeader "Access-Control-Allow-Headers" "content-type"
         post "/" $ do
+            setHeader "Access-Control-Allow-Origin" "*"
             lppRequest <- jsonData :: ActionM LPP
             json $ accumulateStates (getInitialState lppRequest)
